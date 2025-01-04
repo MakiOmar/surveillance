373a899d59a824ab78f8d3e3c8fb818638cc5ef7
makiomar <mo7amed.maki@gmail.com>
2025-01-04 13:43:47 +0200

    Fix end button request
    use data-hx-vals

--- views/patients-table.php
+++ views/patients-table.php
@@ -38,8 +38,8 @@ if ( ! empty( $patients ) ) : ?>
 					<td>
 						<button 
 							class="btn btn-sm <?php echo esc_html( 'under_surveillance' === $patient->status ? 'btn-danger' : 'btn-success' ); ?>" 
-							data-hx-post="<?php echo esc_url( admin_url( 'admin-ajax.php?action=toggle_surveillance_status' ) ); ?>" 
-							data-hx-include="[name=id]" 
+							data-hx-post="<?php echo esc_url( admin_url( 'admin-ajax.php?action=toggle_surveillance_status') ); ?>" 
+							data-hx-vals='{"id": "<?php echo esc_js( $patient->id ); ?>"}'
 							data-hx-target="closest tr" 
 							data-hx-swap="outerHTML"
 							hx-indicator="#maglev-loading-indicator"
@@ -47,7 +47,6 @@ if ( ! empty( $patients ) ) : ?>
 							<?php echo esc_html( 'under_surveillance' === $patient->status ? 'End' : 'Start' ); ?>
 						</button>
 						<a class="btn btn-sm btn-warning" href="<?php echo esc_url( add_query_arg( 'patient', $patient->id, home_url( '/line-list' ) ) ); ?>">Line list</a>
-						<input type="hidden" name="id" value="<?php echo esc_attr( $patient->id ); ?>">
 					</td>
 				</tr>
 			<?php endforeach; ?>
