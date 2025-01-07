7ea13675ee79b9ee170a8955c6638a5ad36f500d
makiomar <mo7amed.maki@gmail.com>
2025-01-07 14:48:22 +0200

    Ability to set created at

--- http/models/SurveillanceDeviceBundle.php
+++ http/models/SurveillanceDeviceBundle.php
@@ -1,19 +1,43 @@
-<?php
-// Exit if accessed directly.
+<?php //phpcs:disable WordPress.Files.FileName.NotHyphenatedLowercase
+/**
+ * Surveillance Device Bundle Model
+ *
+ * @package PluginNamespace
+ */
+
 defined( 'ABSPATH' ) || exit;
 
 use Dbout\WpOrm\Orm\AbstractModel;
 
+/**
+ * Class SurveillanceDeviceBundle
+ *
+ * Represents the surveillance device bundle model.
+ */
 class SurveillanceDeviceBundle extends AbstractModel {
 
-	protected $table    = 'surveillance_device_bundle';
+	/**
+	 * The database table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'surveillance_device_bundle';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = array(
 		'surveillance_devices_id',
 		'bundle_care',
+		'created_at',
 	);
 
 	/**
-	 * Surveillance Device relationship
+	 * Relationship: Surveillance Device
+	 *
+	 * Defines a relationship with the SurveillanceDevicesModel.
 	 *
 	 * @return mixed
 	 */
