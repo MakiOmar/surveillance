2d7290110c726306dd81d7b49872ce5e0b10bcbc
makiomar <mo7amed.maki@gmail.com>
2025-01-07 11:56:47 +0200

    Arrange

--- views/partials/bundlecare/1.php
+++ views/partials/bundlecare/1.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Device 1 bundle care
+ * Device 1 bundle care form
  *
  * @package surv
  */
@@ -12,62 +12,59 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<form>
-	<table class="bundlecare">
-		<thead>
-			<tr>
-				<th rowspan="2">Date</th>
-				<th colspan="3">Hand Hygiene</th>
-				<th colspan="3">Daily assessment of catheter necessity</th>
-				<th colspan="3">Proper dressing choice</th>
-				<th colspan="3">* Proper frequency of dressing change</th>
-				<th colspan="3">** Proper replacement of administrative sets</th>
-			</tr>
-			<tr>
-				<th>YES</th>
-				<th>No</th>
-				<th>N/A</th>
-				<th>YES</th>
-				<th>No</th>
-				<th>N/A</th>
-				<th>YES</th>
-				<th>No</th>
-				<th>N/A</th>
-				<th>YES</th>
-				<th>No</th>
-				<th>N/A</th>
-				<th>YES</th>
-				<th>No</th>
-				<th>N/A</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>
-					<input type="date" name="date">
-				</td>
-				<!-- Hand Hygiene -->
-				<td><input type="radio" name="hand_hygiene" value="YES"></td>
-				<td><input type="radio" name="hand_hygiene" value="No"></td>
-				<td><input type="radio" name="hand_hygiene" value="N/A"></td>
-				<!-- Catheter Necessity -->
-				<td><input type="radio" name="catheter_assessment" value="YES"></td>
-				<td><input type="radio" name="catheter_assessment" value="No"></td>
-				<td><input type="radio" name="catheter_assessment" value="N/A"></td>
-				<!-- Proper Dressing Choice -->
-				<td><input type="radio" name="dressing_choice" value="YES"></td>
-				<td><input type="radio" name="dressing_choice" value="No"></td>
-				<td><input type="radio" name="dressing_choice" value="N/A"></td>
-				<!-- Frequency of Dressing Change -->
-				<td><input type="radio" name="dressing_frequency" value="YES"></td>
-				<td><input type="radio" name="dressing_frequency" value="No"></td>
-				<td><input type="radio" name="dressing_frequency" value="N/A"></td>
-				<!-- Replacement of Administrative Sets -->
-				<td><input type="radio" name="replacement_sets" value="YES"></td>
-				<td><input type="radio" name="replacement_sets" value="No"></td>
-				<td><input type="radio" name="replacement_sets" value="N/A"></td>
-			</tr>
-		</tbody>
-	</table>
-	<button type="submit">Submit</button>
-</form>
+<table class="bundlecare">
+	<thead>
+		<tr>
+			<th rowspan="2">Date</th>
+			<th colspan="3">Hand Hygiene</th>
+			<th colspan="3">Daily assessment of catheter necessity</th>
+			<th colspan="3">Proper dressing choice</th>
+			<th colspan="3">* Proper frequency of dressing change</th>
+			<th colspan="3">** Proper replacement of administrative sets</th>
+		</tr>
+		<tr>
+			<th>YES</th>
+			<th>No</th>
+			<th>N/A</th>
+			<th>YES</th>
+			<th>No</th>
+			<th>N/A</th>
+			<th>YES</th>
+			<th>No</th>
+			<th>N/A</th>
+			<th>YES</th>
+			<th>No</th>
+			<th>N/A</th>
+			<th>YES</th>
+			<th>No</th>
+			<th>N/A</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				<input type="date" name="date">
+			</td>
+			<!-- Hand Hygiene -->
+			<td><input type="radio" name="hand_hygiene" value="YES" required></td>
+			<td><input type="radio" name="hand_hygiene" value="No"></td>
+			<td><input type="radio" name="hand_hygiene" value="N/A"></td>
+			<!-- Catheter Necessity -->
+			<td><input type="radio" name="catheter_assessment" value="YES" required></td>
+			<td><input type="radio" name="catheter_assessment" value="No"></td>
+			<td><input type="radio" name="catheter_assessment" value="N/A"></td>
+			<!-- Proper Dressing Choice -->
+			<td><input type="radio" name="dressing_choice" value="YES" required></td>
+			<td><input type="radio" name="dressing_choice" value="No"></td>
+			<td><input type="radio" name="dressing_choice" value="N/A"></td>
+			<!-- Frequency of Dressing Change -->
+			<td><input type="radio" name="dressing_frequency" value="YES" required></td>
+			<td><input type="radio" name="dressing_frequency" value="No"></td>
+			<td><input type="radio" name="dressing_frequency" value="N/A"></td>
+			<!-- Replacement of Administrative Sets -->
+			<td><input type="radio" name="replacement_sets" value="YES" required></td>
+			<td><input type="radio" name="replacement_sets" value="No"></td>
+			<td><input type="radio" name="replacement_sets" value="N/A"></td>
+		</tr>
+	</tbody>
+</table>
---
+++ views/partials/bundlecare/form.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Bundel care form
+ *
+ * @package surv
+ */
+
+/**
+ * Prevent direct access to the file.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<form
+	hx-post="<?php echo esc_url( admin_url( 'admin-ajax.php?action=insert_bundle_care' ) ); ?>"
+	hx-swap="none"
+	hx-indicator="#maglev-loading-indicator"
+	hx-confirm="Are you sure?"
+	>
+	<?php load_view( 'partials/bundlecare/' . $device_id ); ?>
+	<input type="hidden" name="surveillance_device_id" value="<?php echo esc_attr( $surveillance_device_id ); ?>">
+	<?php wp_nonce_field( 'insertbundlecare_nonce', '_wpnonce' ); ?>
+	<button type="submit" class="btn btn-success mt-2">Submit</button>
+</form>
